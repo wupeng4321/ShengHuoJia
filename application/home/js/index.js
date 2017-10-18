d1991b75e6a59cb08b652f4f5d0201e68d936884
wupeng4321 <wupeng_4321@163.com>
2017-10-18 08:50:17 +0800

    添加商品类目

--- application/home/js/index.js
+++ application/home/js/index.js
@@ -2,10 +2,12 @@
  * Created by apple on 2017/10/17.
  */
 $(document).ready(function () {
+
+
+    // 用于轮播图
     var ulDom = $(".bannerDiv>ul");
     var leftNum = parseInt(ulDom.css("left"));
     var width = parseInt($(".bannerDiv>ul>li>img").css("width"));
-
     var timerLeft = setInterval(function () {
         left();
     }, 2500);
@@ -26,5 +28,11 @@ $(document).ready(function () {
         timerLeft = setInterval(left, 2500);
     });
 
+    //用于类目点击
+    var liDom = $(".category>ul>li");
+    liDom.click(function () {
+        alert($(this).index());
+        liDom.attr("href","../goods/goodsList.html?type=3");
+    });
 
 });
